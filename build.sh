7c0fa81dda1778261a4d8f49aa383da83707b1fb
jsq1234 <mananprakash3078@gmail.com>
2024-02-29 10:08:25 +0530

    changed build file to clean the docker images, run maven and dockerize the jar files. Also removed tests for the time being

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
 
+echo "$(tput bold)$(tput setaf 6)Removing existing images...$(tput sgr0)"
+./clean.sh
+
+echo "$(tput bold)$(tput setaf 6)Rebuilding packages using mvnw$(tput sgr0)" 
+./mvnw clean package --threads 8
+
 echo "$(tput bold)$(tput setaf 6)Creating docker image for gateway$(tput sgr0)"
 docker build -t gateway-image ./gateway
 
